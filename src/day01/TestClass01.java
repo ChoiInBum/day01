bc590c1a0808c2cc7d6c6430b0683e8c63829553
ciw <ciw@n.com>
2023-06-12 15:42:29 +0900

    day01 플젝 완성

---
+++ src/day01/TestClass01.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class TestClass01 {
+
+	public static void main(String[] args) {
+		System.out.print("1. Hello");
+		System.out.print("2. 안녕");
+		System.out.print("3. Hi");
+		System.out.print("4. 안녕하세요");
+	}
+
+}
